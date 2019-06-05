f1cff9e6717fe49d2f263f574091370cded82b6b
AleksandrKostik <alexander.kostik@devexpress.com>
2019-06-05 16:37:21 +0300

    ImageTilesLayer.DataProvider is changed to Bing Maps

--- CS/Form1.cs
+++ CS/Form1.cs
@@ -18,7 +18,7 @@ namespace AddItemsManually {
 
             // Create a layer to load image tiles from OpenStreetMap.
             ImageTilesLayer tileLayer = new ImageTilesLayer();
-            tileLayer.DataProvider = new OpenStreetMapDataProvider();
+            tileLayer.DataProvider = new BingMapDataProvider();
             map.Layers.Add(tileLayer);
 
             // Create a layer to display vector items.
